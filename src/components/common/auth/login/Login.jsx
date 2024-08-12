7ee9959f046b8b3866b650469f04c03f1ed6187d
thivagarradhakrishnan <ajithivagar@gmail.com>
2024-08-12 14:43:59 +0530

    hii

--- src/components/common/auth/login/Login.jsx
+++ src/components/common/auth/login/Login.jsx
@@ -1,162 +1,13 @@
-// 'use client';
-// import React, { useState } from "react";
-// import '@styles/common/auth/login.css';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
-// import Link from "next/link";
-// import { useRouter } from 'next/navigation';
-// import { UserAuth } from "@context/AuthContext";
-
-// import Box from '@mui/material/Box';
-// import TextField from '@mui/material/TextField';
-
-// const Login = () => {
-//     const [email, setEmail] = useState('');
-//     const [password, setPassword] = useState('');
-//     const [showPassword, setShowPassword] = useState(false);
-//     const [emailError, setEmailError] = useState('');
-//     const [passwordError, setPasswordError] = useState('');
-//     const [generalError, setGeneralError] = useState('');
-
-//     const { googleSignIn, signInWithEmail } = UserAuth();
-//     const router = useRouter();
-
-//     const validateEmail = (email) => {
-//         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//         return emailRegex.test(email);
-//     };
-
-//     const handleEmailChange = (e) => {
-//         setEmail(e.target.value);
-//         setEmailError(''); // Clear email error on change
-//         setGeneralError(''); // Clear general error on change
-//     };
-
-//     const handlePasswordChange = (e) => {
-//         setPassword(e.target.value);
-//         setPasswordError(''); // Clear password error on change
-//         setGeneralError(''); // Clear general error on change
-//     };
-
-//     const handleSubmit = async (e) => {
-//         e.preventDefault();
-//         setGeneralError('');
-//         setEmailError('');
-//         setPasswordError('');
-//         let valid = true;
-
-//         if (!email || !validateEmail(email)) {
-//             setEmailError('Invalid email address');
-//             valid = false;
-//         }
-
-//         if (!password) {
-//             setPasswordError('Password is required');
-//             valid = false;
-//         }
-
-//         if (!valid) {
-//             return;
-//         }
-
-//         try {
-//             await signInWithEmail(email, password);
-//             router.push('/');
-//         } catch (error) {
-//             if (error.code === 'auth/user-not-found' || error.code === 'auth/wrong-password') {
-//                 setGeneralError("Email or password is incorrect. Please try again");
-//             } else {
-//                 setGeneralError("Email or password is incorrect. Please try again");
-//             }
-//         }
-//     };
-
-//     const togglePasswordVisibility = () => {
-//         setShowPassword(!showPassword);
-//     };
-
-//     const handleGoogleSignIn = async () => {
-//         try {
-//             await googleSignIn();
-//             router.push('/');
-//         } catch (error) {
-//             setGeneralError(error.message);
-//         }
-//     };
-
-//     return (
-//         <div className="login">
-//             <div className="login-container">
-//                 <div className="login-heading">
-//                     <h1>Login</h1>
-//                     {generalError && <div className="error-message" style={{ paddingTop: "6px" }}>{generalError}</div>}
-//                 </div>
-//                 <form className="form" onSubmit={handleSubmit}>
-//                     <Box component="div" noValidate autoComplete="off" className="email">
-//                         <TextField
-//                             id="outlined-email"
-//                             label="Email"
-//                             variant="outlined"
-//                             className="custom-text-field"
-//                             error={!!emailError}
-//                             helperText={emailError}
-//                             value={email}
-//                             onChange={handleEmailChange}
-//                         />
-//                     </Box>
-
-//                     <Box component="div" noValidate autoComplete="off" className="password">
-//                         <TextField
-//                             id="outlined-password"
-//                             label="Password"
-//                             variant="outlined"
-//                             type={showPassword ? "text" : "password"}
-//                             className="custom-text-field"
-//                             error={!!passwordError}
-//                             helperText={passwordError}
-//                             value={password}
-//                             onChange={handlePasswordChange}
-//                             InputProps={{
-//                                 endAdornment: (
-//                                     <span className="eye" onClick={togglePasswordVisibility}>
-//                                         <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
-//                                     </span>
-//                                 ),
-//                             }}
-//                         />
-//                         <div className="forgot-password" style={{ width: "100%", display: "flex", justifyContent: "end", fontSize: "12px", fontFamily: "Inter", paddingTop: "6px" }}>
-//                             <Link href="/forgot-password">Forgot Password?</Link>
-//                         </div>
-//                     </Box>
-
-//                     <div className="login-button">
-//                         <button className="login-btn" type="submit">Login</button>
-//                     </div>
-
-//                     <div className="google-signin">
-//                         <button type="button" className="login-with-google-btn" onClick={handleGoogleSignIn}>Login with Google</button>
-//                         <p style={{ fontFamily: "Inter", fontSize: "13px", paddingTop: "16px", textAlign: "center" }}>Don&apos;t have an account? <Link href="/signup">Sign up</Link></p>
-//                     </div>
-//                 </form>
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default Login;
-
-
-
-
 'use client';
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import '@styles/common/auth/login.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import Link from "next/link";
-import { useRouter, useSearchParams } from 'next/navigation';
-import { UserAuth } from "@context/AuthContext";
-
+import { useRouter } from 'next/navigation';
+import { GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword } from "firebase/auth";
+import { ref, set } from 'firebase/database';
+import { auth, database } from '@firebase'; // Adjust this path based on your actual file structure
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
@@ -167,20 +18,8 @@ const Login = () => {
     const [emailError, setEmailError] = useState('');
     const [passwordError, setPasswordError] = useState('');
     const [generalError, setGeneralError] = useState('');
-    const [returnUrl, setReturnUrl] = useState('/'); // Default return URL
 
     const router = useRouter();
-    const searchParams = useSearchParams();
-
-    useEffect(() => {
-        if (router.isReady) {
-            const returnUrlParam = searchParams.get('returnUrl');
-            console.log('Return URL:', returnUrlParam); // Debugging
-            if (returnUrlParam) {
-                setReturnUrl(returnUrlParam);
-            }
-        }
-    }, [searchParams, router.isReady]);
 
     const validateEmail = (email) => {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -221,9 +60,18 @@ const Login = () => {
         }
 
         try {
-            await signInWithEmail(email, password);
-            console.log('Redirecting to:', returnUrl); // Debugging
-            router.push(returnUrl); // Redirect to the return URL
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;
+
+            // Store user data in Firebase Realtime Database
+            const userRef = ref(database, 'usersData/' + user.uid);
+            await set(userRef, {
+                uid: user.uid,
+                email: user.email,
+                firstName: user.email.split('@')[0],
+            });
+
+            router.push('/');
         } catch (error) {
             if (error.code === 'auth/user-not-found' || error.code === 'auth/wrong-password') {
                 setGeneralError("Email or password is incorrect. Please try again");
@@ -239,11 +87,25 @@ const Login = () => {
 
     const handleGoogleSignIn = async () => {
         try {
-            await googleSignIn();
-            console.log('Redirecting to:', returnUrl); // Debugging
-            router.push(returnUrl); // Redirect to the return URL
-        } catch (error) {
-            setGeneralError(error.message);
+            const provider = new GoogleAuthProvider();
+            const result = await signInWithPopup(auth, provider);
+            const user = result.user;
+
+            // Store user data in Firebase Realtime Database
+            const userRef = ref(database, 'usersData/' + user.uid);
+            await set(userRef, {
+                uid: user.uid,
+                email: user.email,
+                firstName: user.email.split('@')[0],
+            });
+
+            router.push('/');
+        } catch (err) {
+            if (err.code === 'auth/cancelled-popup-request') {
+                setGeneralError('Unable to get profile information from Google.');
+            } else if (err.code !== 'auth/popup-closed-by-user') {
+                setGeneralError(err.message);
+            }
         }
     };
 
