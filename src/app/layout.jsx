bb86dc12b6cedd3bfd256a425a5f376d373e2b14
aishvarya <aishvarya@trafyai.com>
2024-08-23 07:45:36 +0000

    h

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,12 @@
+// app/layout.jsx
+
 import "@styles/globals.css";
 import Header from "@components/common/header/Header.jsx";
 import Footer from "@components/common/footer/Footer";
 import imageO from "../app/opengraph-image.png";
 import { AuthContextProvider } from "@context/AuthContext";
 import { CourseContextProvider } from "@context/CourseContext";
-import { GoogleTagManager } from "@components/GoogleTagManager";
+import Script from "next/script";
 
 export const metadata = {
   title: "trafy - Your Personalised AI mentor",
@@ -25,12 +27,35 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NX8D4BFD');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NX8D4BFD"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <AuthContextProvider>
           <CourseContextProvider>
             <main className="app">
               <Header />
-              <GoogleTagManager />
               {children}
               <Footer />
             </main>
