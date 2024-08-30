446480018e595f44c75199898e93e33d6f9af686
thivagarradhakrishnan <ajithivagar@gmail.com>
2024-08-30 17:35:08 +0530

    hii

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -37,7 +37,7 @@ export default function RootLayout({ children }) {
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-NX8D4BFD');
         `}
         </Script>
@@ -45,11 +45,11 @@ export default function RootLayout({ children }) {
       <body>
    
    
-        <noscript>
+        {/* <noscript>
           <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NX8D4BFD"
         height="0" width="0" style="display:none;visibility:hidden">
           </iframe>
-        </noscript>
+        </noscript> */}
 
 
 
@@ -61,7 +61,7 @@ export default function RootLayout({ children }) {
               {children}
               <Footer />
             </main>
-            {/* <GoogleAnalytics gaId="GTM-NX8D4BFD" /> */}
+            <GoogleAnalytics gaId="GTM-NX8D4BFD" />
           </CourseContextProvider>
         </AuthContextProvider>
       </body>
@@ -71,5 +71,6 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
 
 
