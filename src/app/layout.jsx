f1bfb4ff33ae5ab3954655a64b918045fafb7963
thivagarradhakrishnan <ajithivagar@gmail.com>
2024-10-03 15:08:45 +0530

    payment

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -35,7 +35,7 @@ export default function RootLayout({ children }) {
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NX8D4BFD');`
         }}
       />
-      <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/40350552.js"/>
+      
           </head>
       <body>
 
